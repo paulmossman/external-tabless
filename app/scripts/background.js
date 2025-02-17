fd8877e11cc49316ce58233ccb937820e0459416
Paul Mossman <paul.mossman@gmail.com>
2025-02-17 14:15:19 -0500

    Functionality

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,27 +1,85 @@
 'use strict';
 
-// Extension functionality
+function waitForOneUrl(tab) {
+  return new Promise((resolve, reject) => {
+    // If the tab already has a URL, return it now.
+    if (tab.pendingUrl !== undefined || tab.url !== undefined) {
+      resolve(tab);
+      return;
+    }
+
+    const startTime = Date.now();
+    const timeout = 1.5 * 1000; // Timeout
+
+    function checkUrls() {
+      // Refresh tab data
+      chrome.tabs.get(tab.id, (updatedTab) => {
+        if (updatedTab.pendingUrl !== undefined || updatedTab.url !== undefined) {
+          resolve(updatedTab); // At least one URL is defined
+          return;
+        }
+
+        if (Date.now() - startTime > timeout) {
+          reject(new Error('Timeout: Neither URL was defined.'));
+          return;
+        }
+
+        setTimeout(checkUrls, 50); // Check again after a small delay
+      });
+    }
+
+    checkUrls(); // Start checking
+  });
+}
 
 var detachTab = function(tab) {
-  if (tab.index === 0 || tab.pinned) {
+  detachTabAsync(tab).then();
+};
+
+var detachTabAsync = async function(tab) {
+
+  if (tab.openerTabId != undefined) {
+    // Definately opened just now from inside Chrome,
+    // so do *not* open in a new window.
     return;
   }
 
-  chrome.windows.get(tab.windowId, function(oldWindow) {
-    chrome.windows.create({tabId: tab.id, incognito: tab.incognito, state: oldWindow.state});
-  });
-};
+  if( tab.index == 0 ) {
+    // Already a new window (first tab), so do *not* open in a new window.
+    return;
+  }
 
-var detachAllTabs = function() {
-  chrome.tabs.query({}, function(tabs) {
-    tabs.forEach(detachTab);
+  var tabWithUrl = undefined;
+  try {
+    tabWithUrl = await waitForOneUrl(tab);
+  } catch (e) {
+    // No URL, so do *not* open in a new window
+    return;
+  }
+
+  if(tabWithUrl.pendingUrl == undefined) {
+    // Already opened.  i.e. When the Extension loads.
+    // So do *not* open in a new window.
+    return;
+  }
+
+  // Otheriwse, open a new window!
+  chrome.windows.get(tab.windowId, function(oldWindow) {
+
+    chrome.windows.create(
+      { tabId: tab.id, incognito: tab.incognito, state: oldWindow.state },
+      function (newWindow) {
+        // Draw attention to it.  (Sometimes focus moves to the window in
+        // another desktop.)
+        chrome.windows.update(newWindow.id, { drawAttention: true });
+      }
+    );
   });
 };
 
 var startTabless = function() {
   chrome.storage.local.set({tabless: true});
   chrome.tabs.onCreated.addListener(detachTab);
-  detachAllTabs();
   chrome.action.setIcon({
     path: {
       16: '../images/external-tabless-16.png',
@@ -33,51 +91,7 @@ var startTabless = function() {
   });
 };
 
-var stopTabless = function() {
-  chrome.storage.local.set({tabless: false});
-  chrome.tabs.onCreated.removeListener(detachTab);
-  chrome.action.setIcon({
-    path: {
-      16: '../images/external-tabless-16.png',
-      19: '../images/external-tabless-19.png',
-      32: '../images/external-tabless-32.png',
-      38: '../images/external-tabless-38.png',
-      64: '../images/external-tabless-64.png',
-    }
-  });
-};
-
-var toggleTabless = function() {
-  chrome.storage.local.get(['tabless'], function(result) {
-    if(result.tabless) {
-      stopTabless();
-    } else {
-      startTabless();
-    }
-  });
-};
-
-chrome.action.onClicked.addListener(toggleTabless);
-
-chrome.runtime.onInstalled.addListener(stopTabless);
-
-chrome.runtime.onStartup.addListener(function() {
-  chrome.storage.local.get(['tabless'], function(result) {
-    if(result.tabless) {
-      startTabless();
-    } else {
-      stopTabless();
-    }
-  });
-});
-
-chrome.storage.local.get(['tabless'], function(result) {
-  if(result.tabless) {
-    startTabless();
-  } else {
-    stopTabless();
-  }
-});
+startTabless();
 
 // Keep alive
 // Created by wOxxOm
@@ -118,7 +132,7 @@ async function keepAlive() {
   chrome.tabs.onUpdated.addListener(retryOnTabUpdate);
 }
 
-async function retryOnTabUpdate(tabId, info) {
+async function retryOnTabUpdate(tabId, info) {  
   if (info.url && /^(file|https?):/.test(info.url)) {
     keepAlive();
   }
